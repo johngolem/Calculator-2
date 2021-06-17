ed179ec54475db018b89604109efc1e06f8245e1
john paul <jpaulopiy@gmail.com>
2021-06-17 16:38:24 +0300

    got the calc to function perfectly, need to DRY the code

--- js/scripts.js
+++ js/scripts.js
@@ -24,3 +24,32 @@ var add = function(number1, number2) {
     $("#output").text(result);
         });
     });
+    $(document).ready(function() {
+        $("form#subtract").submit(function(event) {
+        event.preventDefault();
+        var number1 = parseInt($("#subtract1").val());
+        var number2 = parseInt($("#subtract2").val());
+        var result = subtract(number1, number2);
+        $("#output").text(result);
+            });
+        });
+    $(document).ready(function() {
+        $("form#multiply").submit(function(event) {
+        event.preventDefault();
+        var number1 = parseInt($("#multiply1").val());
+        var number2 = parseInt($("#multiply2").val());
+        var result = multiply(number1, number2);
+        $("#output").text(result);
+        });
+    });
+    $(document).ready(function() {
+        $("form#divide").submit(function(event) {
+        event.preventDefault();
+        var number1 = parseInt($("#divide1").val());
+        var number2 = parseInt($("#divide2").val());
+        var result = divide(number1, number2);
+        $("#output").text(result);
+            });
+        });
+    
+    
